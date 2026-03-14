2ff7677f0e1c5c3ea1501661bb95aec9226f0cfd
Anzo <antonr@live.dk>
2026-03-14 14:26:23 +0000

    Update version and refactor style toggle logic

--- hide_dropped_shows.js
+++ hide_dropped_shows.js
@@ -4,7 +4,7 @@
 // @match       https://anilist.co/search/anime*
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACHklEQVQ4T5VTX0hTcRT+zu7drsp0i9hsYxVmD6WbEEVFD0I9BL4HEfUUhEUZlRquPxhBBUUGaUTLoJZSQgRCxBLKKEYUGwaZD4EaMfAlYkLpuN57T/f3g81mf7YO3IfL73zf+c4536FQfaTFZLoM5kYAZH/lBIPoo0J8kgJrmj7Y4HA5qN9yiMYpUBexllRmTdPIQQ7oCzoMckocGbk/1WBBwPkXy7IQCa9HtOMoaqrdeJr+hPMzfnBlDTyJXmjTaVt5cZdFBIZpovPYIXQePyI5x7/p2J5U8XWe4B46herUMKCoRUoKBMyMZV4P4v292LRxg0zKmYy9b4HHGZQmENWbt21F/E6fBGdnZxEMrEBsCmhNcWkC0f/Z6Akcbt2PVHoMo6+SspWp74wto4zc3dN/byEvf+h+zB5iAwYePEJi5Dn6rl2C2+PF7jeMkav/mIGQv3NHM2I3elBVVYlsNosfc/Oo9fugqioGP1s42F5CwZWL3di3ZxcMw4CuLxQmLQhFGy3xMUx+yYAcCtgy4U0OwpmZANWubuRQMICHtvy19XV48fI1eq7fkgRN4XU409UuVR14D/RPL27QPRCVMyHfqgbebK+tq6MNFZoLN2/fw/CTZzLT71uOC+eiWBkKIjHD6J4oeE4aq2LyHaSVFUUhl9MlQcK+pj2TfNi2hrC1iDljkcC2tvwp65jElpZaWDKKY/rlnP/vIm2wOOefuwrlqMNkePsAAAAASUVORK5CYII=
 // @grant       none
-// @version     1.1.1
+// @version     1.1.2
 // @author      AnzoDK
 // @downloadURL https://github.com/AnzoDK/Anilist-Dropped-Show-Hider/releases/latest/download/hide_dropped_shows.js
 // @description 14.3.2026, 11.51.46
@@ -96,20 +96,23 @@ function ToggleDroppedShows()
   g_toggled = !g_toggled;
   g_buttonElement.style.backgroundColor = g_toggled ? g_activeColor : g_disabledColor;
   ToggleStyle(g_toggled);
-  
-  
+
+
 }
 
 function ToggleStyle(state)
 {
   var head = document.head || document.getElementsByTagName('head')[0];
-  if(!state && g_styleElement != null)
+  if(!state)
   {
-    head.removeChild(g_styleElement);
-    g_styleElement = null;
+    if(g_styleElement != null)
+      {
+        head.removeChild(g_styleElement);
+        g_styleElement = null;
+      }
     return;
   }
-  
+
   var style = document.createElement("style");
   style.type = 'text/css';
 
@@ -122,10 +125,10 @@ function ToggleStyle(state)
   }
   else
   {
-    style.appendChild(document.createTextNode(css));
+    var e = style.appendChild(document.createTextNode(css));
   }
 
-  
+
   head.appendChild(style);
   g_styleElement = style;
 }
@@ -136,7 +139,7 @@ function SetUp_CreateHideButton()
   btn.id = "toggleDroppedBtn";
   btn.appendChild(document.createTextNode("Toggle Dropped Shows!!"));
   btn.onclick = ToggleDroppedShows;
-  btn.style.backgroundColor = "#3db4f2";
+  btn.style.backgroundColor = g_toggled ? g_activeColor : g_disabledColor; ;
   btn.style.color = "white";
   btn.style.borderRadius = "6px";
   document.getElementsByClassName("filters")[0].appendChild(btn);
@@ -146,8 +149,8 @@ function SetUp_CreateHideButton()
 function SetUp_DroppedHider_Styles()
 {
 
-  ToggleStyle(true);
-  
+  ToggleStyle(g_toggled);
+
   console.log("DroppedHider - Styles initilized");
 
 }
