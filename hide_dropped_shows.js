c90fc174d3c80a13285993720b001dbb8a4b14a6
Anzo <antonr@live.dk>
2026-04-21 13:55:32 +0000

    Made settings button prettier

--- hide_dropped_shows.js
+++ hide_dropped_shows.js
@@ -4,7 +4,7 @@
 // @match       https://anilist.co/search/anime*
 // @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACHklEQVQ4T5VTX0hTcRT+zu7drsp0i9hsYxVmD6WbEEVFD0I9BL4HEfUUhEUZlRquPxhBBUUGaUTLoJZSQgRCxBLKKEYUGwaZD4EaMfAlYkLpuN57T/f3g81mf7YO3IfL73zf+c4536FQfaTFZLoM5kYAZH/lBIPoo0J8kgJrmj7Y4HA5qN9yiMYpUBexllRmTdPIQQ7oCzoMckocGbk/1WBBwPkXy7IQCa9HtOMoaqrdeJr+hPMzfnBlDTyJXmjTaVt5cZdFBIZpovPYIXQePyI5x7/p2J5U8XWe4B46herUMKCoRUoKBMyMZV4P4v292LRxg0zKmYy9b4HHGZQmENWbt21F/E6fBGdnZxEMrEBsCmhNcWkC0f/Z6Akcbt2PVHoMo6+SspWp74wto4zc3dN/byEvf+h+zB5iAwYePEJi5Dn6rl2C2+PF7jeMkav/mIGQv3NHM2I3elBVVYlsNosfc/Oo9fugqioGP1s42F5CwZWL3di3ZxcMw4CuLxQmLQhFGy3xMUx+yYAcCtgy4U0OwpmZANWubuRQMICHtvy19XV48fI1eq7fkgRN4XU409UuVR14D/RPL27QPRCVMyHfqgbebK+tq6MNFZoLN2/fw/CTZzLT71uOC+eiWBkKIjHD6J4oeE4aq2LyHaSVFUUhl9MlQcK+pj2TfNi2hrC1iDljkcC2tvwp65jElpZaWDKKY/rlnP/vIm2wOOefuwrlqMNkePsAAAAASUVORK5CYII=
 // @grant       none
-// @version     1.2.1
+// @version     1.2.2
 // @author      AnzoDK
 // @downloadURL https://github.com/AnzoDK/Anilist-Dropped-Show-Hider/releases/latest/download/hide_dropped_shows.js
 // @description 14.3.2026, 11.51.46
@@ -186,6 +186,8 @@ function TagUntaggedShows()
 
 function SetUp_CreateHideButton() //This function keeps running itself infinitely??????
 {
+  var btnDiv = document.createElement("div");
+  btnDiv.id = "toggleHiddenWrapper";
   var btn = document.createElement("button");
   btn.id = "toggleHiddenBtn";
   btn.appendChild(document.createTextNode("Toggle Hidden Shows!!"));
@@ -200,7 +202,8 @@ function SetUp_CreateHideButton() //This function keeps running itself infinitel
     setTimeout(SetUp_CreateHideButton,2000);
     return;
   }
-  filters.appendChild(btn);
+  btnDiv.appendChild(btn);
+  filters.appendChild(btnDiv);
   g_buttonElement = btn;
   console.log("DroppedHider - Button initilized");
 }
@@ -290,6 +293,9 @@ function SetUp_CreateHideButtonSettings()
       return;
     }
   var settingsBtn = document.createElement("a");
+  settingsBtn.style.position = "relative";
+  settingsBtn.style.top = "-32px";
+  settingsBtn.style.left = "5px";
   settingsBtn.classList.add("filter");
   settingsBtn.classList.add("select");
   var svg = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon svg-inline--fa fa-cog fa-w-16 fa-fw"><path data-v-04b245e6="" fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z" class=""></path></svg>';
@@ -298,7 +304,8 @@ function SetUp_CreateHideButtonSettings()
   //settingsBtn.appendChild(document.createTextNode(svg));
   settingsBtn.href = "#";
   settingsBtn.onclick = ToggleSettingsModal;
-  hideBtn.parentNode.insertBefore(settingsBtn, hideBtn);
+  //hideBtn.parentNode.insertBefore(settingsBtn, hideBtn);
+  hideBtn.parentNode.appendChild(settingsBtn);
   g_settingsBtnElement = settingsBtn;
 }
 
